2dd8b83f8684f0b2910b0eab1a3e435757f2d86a
app2 <isucon@example.com>
2017-10-21 15:16:27 +0900

    initial commit

---
+++ db/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/env.sh
+
+DB_DIR=$(cd $(dirname $0) && pwd)
+cd $DB_DIR
+
+mysql -uroot -e "DROP DATABASE IF EXISTS isubata; CREATE DATABASE isubata;"
+mysql -uroot isubata < ./isubata.sql
